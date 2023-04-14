22260e3ddad2ee8dd5059bda55db6cb764c8435f
sun baoqing <baoqing.sun@colossal-tech.com>
2023-04-14 14:06:02 +0800

    2021-4-20 20:08:45

--- myzxing/src/main/java/com/ct/myzxing/utils/Utils.java
+++ myzxing/src/main/java/com/ct/myzxing/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.ct.myzxing.utils;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -15,15 +17,20 @@ public class Utils {
     /*
     2023-4-14 11:40:11
     判断是否修改
-    todo 可以由屏幕尺寸入手判断
+    可以由屏幕尺寸入手判断
     * */
     public static boolean modifySize(Context context) {
 
         WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = manager.getDefaultDisplay();
 
+        Point point  = new Point(display.getWidth(), display.getHeight());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealSize(point);
+        }
+
         //todo 需要进一步判断
-        if (display.getHeight() >= 2400){
+        if (point.y >= 2400){
             return true;
         }
 
