be12f37ddcb9b0a99e677aef60e01fa0d703a1fb
baoqing.sun <baoqing.sun@colossal-tech.com>
2024-10-25 16:50:28 +0800

    2024-10-25 16:50:13 补丁

--- myzxing/src/main/java/com/ct/myzxing/camera/CameraManager.java
+++ myzxing/src/main/java/com/ct/myzxing/camera/CameraManager.java
@@ -306,15 +306,6 @@ public final class CameraManager {
             rect.top = rect.top * cameraResolution.x / screenResolution.y;
             rect.bottom = rect.bottom * cameraResolution.x / screenResolution.y;
         }
-        //modify here
-//      rect.left = rect.left * cameraResolution.x / screenResolution.x;
-//      rect.right = rect.right * cameraResolution.x / screenResolution.x;
-//      rect.top = rect.top * cameraResolution.y / screenResolution.y;
-//      rect.bottom = rect.bottom * cameraResolution.y / screenResolution.y;
-        rect.left = rect.left * cameraResolution.y / screenResolution.x;
-        rect.right = rect.right * cameraResolution.y / screenResolution.x;
-        rect.top = rect.top * cameraResolution.x / screenResolution.y;
-        rect.bottom = rect.bottom * cameraResolution.x / screenResolution.y;
         framingRectInPreview = rect;
         //}
         return framingRectInPreview;
